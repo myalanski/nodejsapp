34d2b79d702f7fb2d944bf0ee38546beb4d7f414
Pavel <pavel.holy@dhl.com>
2024-03-03 21:31:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const helper = require('./helper.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 app.get('/employees', async (req, res) => {
     const data = await helper.getDataFromDatabase();
