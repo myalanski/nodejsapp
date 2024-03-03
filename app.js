b130ec1b24590cbb0e7fdc0080d82bd41ca8156f
Pavel <pavel.holy@dhl.com>
2024-03-03 21:30:22 +0100

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const helper = require('./helper.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/employees', async (req, res) => {
+    const data = await helper.getDataFromDatabase();
+    res.send(data);
+    });
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
---
+++ helper.js
@@ -0,0 +1,8 @@
+exports.getDataFromDatabase = function getDataFromDatabase(){
+    const users = {
+        name: 'Jon Smith',
+        email: 'jsmith@gmail.com',
+        title: 'Sales Director'
+        };
+    return users;
+};
