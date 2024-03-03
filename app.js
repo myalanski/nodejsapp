80c69121d49f107c0afe9252ff04d82c88572554
Pavel <pavel.holy@dhl.com>
2024-03-03 22:18:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const helper = require('./helper.js');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.get('/', async (req, res) => {
+  const data = 'api is ok.';
+  res.send(data);
+  });
+
 app.get('/employees', async (req, res) => {
     const data = await helper.getDataFromDatabase();
     res.send(data);
